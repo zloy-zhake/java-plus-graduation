0018cbcc865034c5d4ea69b068e578e5e99d289f
zloy-zhake <jk-jdogg@yandex.ru>
2026-06-11 12:27:49 +0500

    Очистил stats-client: удалил старые классы и зависимости, добавил grpc-client-spring-boot-starter и stats-proto

--- stats/stats-client/src/main/java/ru/practicum/explorewithme/stats/client/StatsServerUnavailable.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.explorewithme.stats.client;
-
-public class StatsServerUnavailable extends RuntimeException {
-    public StatsServerUnavailable(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
